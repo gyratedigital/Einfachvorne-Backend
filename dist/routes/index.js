fee0050a0b1b24343c938fb2c0d295ff5e675a87
Dev <gyratedigital@gmail.com>
2025-07-03 13:34:35 +0500

    git ignore updated

--- dist/routes/index.js
+++
@@ -1,6 +0,0 @@
-export {};
-// export {router as dashboardRouter} from './dashboard.js'
-// export {router as regionRouter} from './region.js'
-// export {router as destinationRouter} from './destination.js'
-// export {router as stripeRouter} from './stripe.js'
-// export {router as resourceRouter} from './resource.js'
